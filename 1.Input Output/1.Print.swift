3a3d231b1815430f6b636f1e0399bd4c3a5ec2df
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-09 10:44:51 +0600

    Input Output

--- 1.Input Output/1.Print.swift
+++ 1.Input Output/1.Print.swift
@@ -3,9 +3,11 @@ print("I am a Programmer")
 
 print("I love to play games")
 
+print("I also like to make games")
+
 /* 
         print() - is the basic output function of swift
 
-        which includes a new after 
+        which includes a new line after after the print string or statement
 
  */
