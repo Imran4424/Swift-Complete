0f09e7029f6f0952fa8e248c5d5d6fd64ecb7504
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 01:30:28 +0600

    Input Output

--- 1.Input Output/4.Separator.swift
+++ 1.Input Output/4.Separator.swift
@@ -6,3 +6,10 @@ print("I", "love", "to", "play", "games", separator: " ", terminator:"\n")
 print("I also like to make games")
 
 
+/* 
+        In the first and second statement, we are adding multiple strings and displaying them using print() function
+
+        the separator takes place between two strings
+
+        terminator decides what character we will put after each print operation
+*/
