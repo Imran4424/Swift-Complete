e2b6c8061aaa65c754257b02c75ebe196a9f9e16
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 02:09:10 +0600

    Input Output

---
+++ 1.Input Output/6.VariableDeclaration.swift
@@ -0,0 +1,16 @@
+
+var intVal = 3
+
+print("value of inVal is", intVal)
+
+var doubleVal = 3.1416
+
+print("value of doubleVal is", doubleVal)
+
+var charVal = 'X'
+
+print("value of charVal is", charVal)
+
+var stringVal = "I am a Programmer"
+
+print(stringVal)
