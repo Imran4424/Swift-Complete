62d1e5dd2e8b3e4bb50194e76681efb7f66ec911
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 02:31:10 +0600

    Input Output

--- 1.Input Output/7.SpecifyingTypes.swift
+++ 1.Input Output/7.SpecifyingTypes.swift
@@ -1,16 +1,16 @@
 
-var intVal:Int = 3
+var intVal: Int = 3
 
 print("value of inVal is", intVal)
 
-var doubleVal:double = 3.1416
+var doubleVal: Double = 3.1416
 
 print("value of doubleVal is", doubleVal)
 
-var charVal = "X"
+var charVal: String = "X"
 
 print("value of charVal is", charVal)
 
-var stringVal = "I am a Programmer"
+var stringVal: String = "I am a Programmer"
 
 print(stringVal)
