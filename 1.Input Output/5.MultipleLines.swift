df136c87fee3b0dad5b85a843065ec4eef64b5a6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 01:49:05 +0600

    Input Output

---
+++ 1.Input Output/5.MultipleLines.swift
@@ -0,0 +1,13 @@
+
+print("I am a Programmer"); print("I love to play games"); print("I also like to make games")
+
+
+/*
+        In, swift putting semicolon (;)  after every statement is not mandatory
+
+        you can put semicolon (;) after every statement but it works really fine without it
+
+        but it is not recomended to put semicolon (;) after every statement
+
+        
+*/
