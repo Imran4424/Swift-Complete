0986208233edb8ab3c2835d6f6c083faa4628a75
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 01:24:35 +0600

    Input Output

--- 1.Input Output/3.NewLineCharacter.swift
+++ 1.Input Output/3.NewLineCharacter.swift
@@ -5,3 +5,11 @@ print("I love to play games\n" , terminator: "")
 
 print("I also like to make games\n", terminator: "")
 
+
+/*
+        Here,
+
+        we are adding new line character(\n) at the end of the string inside the "" (just like c or c++)
+
+        and it's work like a charm
+*/
--- 1.Input Output/4.Separator.swift
+++ 1.Input Output/4.Separator.swift
@@ -4,3 +4,5 @@ print("I", "am", "a", "Programmer", separator: " ")
 print("I", "love", "to", "play", "games", separator: " ", terminator:"\n")
 
 print("I also like to make games")
+
+
