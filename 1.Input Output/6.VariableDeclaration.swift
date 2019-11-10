d80c5379a67eb89a25e65194d7d2f6688553bcb6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 16:30:36 +0600

    Input Output

--- 1.Input Output/6.VariableDeclaration.swift
+++ 1.Input Output/6.VariableDeclaration.swift
@@ -3,6 +3,10 @@ var intVal = 3 // here 3 is  a literal
 
 print("value of inVal is", intVal)
 
+var floatVal = 9.8 // here 3.1416 is a literal
+
+print("value of floatVal is", doubleVal)
+
 var doubleVal = 3.1416 // here 3.1416 is a literal
 
 print("value of doubleVal is", doubleVal)
