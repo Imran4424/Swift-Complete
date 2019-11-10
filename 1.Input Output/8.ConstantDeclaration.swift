9d35872f6a9601bbf35eb855ccc48f8beb09582f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 15:21:43 +0600

    Input Output

--- 1.Input Output/8.ConstantDeclaration.swift
+++ 1.Input Output/8.ConstantDeclaration.swift
@@ -20,5 +20,12 @@ print(PI)
 print(gravity)
 
 /*  
-        
+        declaring constant is really easy in swift
+
+        just use the "let" keyword for declaring constants
+
+        example:
+
+        let PI = 3.1416
+        let gravity: Double = 9.8
 */
