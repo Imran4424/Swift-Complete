57eed26217adc545e8b8a834484224f02283e05b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-11 02:53:25 +0600

    Input Output

--- 1.Input Output/12.IntInput.swift
+++ 1.Input Output/12.IntInput.swift
@@ -1,8 +1,29 @@
 
+// First way
+
 var num: Int? = Int(readLine()!)
 
-print("The entered value is:", num!)
+print("The entered value of the num is:", num!)
+
+// Second way
 
 var money = Int(readLine()!)
 
-print("The entered value is:", num)
+print("The entered value of the money is:", money!)
+
+// Third way
+
+var age: Int
+
+age = Int(readLine()!)
+
+print("The entered value of the age is:", age!)
+
+// Fourth way - Best way
+
+var beans: Int
+
+beans = (Int(readLine()!))!
+
+print("The entered value of the beans is:", beans)
+
