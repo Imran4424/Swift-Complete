901475d19220606cdc3f48ffdfa89189187996d7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 15:44:24 +0600

    Input Output

--- 1.Input Output/9.VarDeclarationWithoutInit.swift
+++ 1.Input Output/9.VarDeclarationWithoutInit.swift
@@ -9,5 +9,8 @@ print(numberCount)
         generally when we declare a variable in swift without any data type specific, it just analyze the initialization value
         and set the variable data type
 
-        but when we declare a variable in swift without any data type specific and without initialization, 
+        but when we declare a variable in swift without any data type specific and without initialization, then compiler doesn't 
+        know what data type it would specify for the uninitialized declared variable
+
+        so it generates an error
 */
