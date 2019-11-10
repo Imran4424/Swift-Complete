75e06a70673f6a333e2d21bca364c4e6e4eaee08
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 16:20:17 +0600

    Input Output

--- 1.Input Output/6.VariableDeclaration.swift
+++ 1.Input Output/6.VariableDeclaration.swift
@@ -28,5 +28,13 @@ print(stringVal)
 
         Now, you may ask what is what the heck is a literal
 
-        literals are 
+        Literal is a value that directly appears in the source code, and used to initialize or assign values of variables
+
+        Literals are different depend upon their type
+
+        3, 7, 11 - these are integer type literal
+        3.1416, 9.8 - these is float or double type literal
+        "X" - this is a character type literal
+        "I am a Programmer" - this is a string type literal
+        true, false - these are boolean type literals
 */
