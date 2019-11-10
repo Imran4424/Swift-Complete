2dbcc57d304fd29ad910edfb5b2cd5bdb6642ea3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 11:21:57 +0600

    Input Output

--- 1.Input Output/6.VariableDeclaration.swift
+++ 1.Input Output/6.VariableDeclaration.swift
@@ -14,3 +14,11 @@ print("value of charVal is", charVal)
 var stringVal = "I am a Programmer"
 
 print(stringVal)
+
+/*  
+        variable declaration in swift is really easy
+
+        var is the basic keyword to declare variables in swift
+
+        variable type will be automatically set upon the value we initialize
+*/
