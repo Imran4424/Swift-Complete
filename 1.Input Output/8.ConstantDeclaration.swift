98715a014dac25d10447fda5b7a492dce7a8127e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-10 14:45:11 +0600

    Input Output

--- 1.Input Output/8.ConstantDeclaration.swift
+++ 1.Input Output/8.ConstantDeclaration.swift
@@ -11,7 +11,7 @@ let gravity: Double = 9.8
 print(PI)
 print(gravity)
 
-// the following lines will
+// the following lines will generate error cause we can't change values of constants
 
 // PI = 3.14
 // gravity = 9.98
@@ -19,4 +19,6 @@ print(gravity)
 print(PI)
 print(gravity)
 
-
+/*  
+        
+*/
