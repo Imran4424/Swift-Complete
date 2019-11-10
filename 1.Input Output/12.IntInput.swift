d5994154a2d66d650756cb69ceb4fd88b84691eb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-11 02:45:41 +0600

    Input Output

--- 1.Input Output/12.IntInput.swift
+++ 1.Input Output/12.IntInput.swift
@@ -1,4 +1,8 @@
 
-var num/* : Int? */ = Int(readLine()!)
+var num: Int? = Int(readLine()!)
+
+print("The entered value is:", num!)
+
+var money = Int(readLine()!)
 
 print("The entered value is:", num)
