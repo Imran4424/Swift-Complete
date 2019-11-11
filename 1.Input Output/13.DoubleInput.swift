6a87c5ee24a6c28039aa996b3a2def7ed650999c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-11 08:18:05 +0600

    Input Output

--- 1.Input Output/13.DoubleInput.swift
+++ 1.Input Output/13.DoubleInput.swift
@@ -2,3 +2,9 @@
 var mark = (Double(readLine()!))!
 
 print("The obtained mark is:", mark)
+
+var cgpa: Double
+
+cgpa = (Double(readLine()!))!
+
+print("The obtained cgpa is:", cgpa)
---
+++ 1.Input Output/14.CharacterInput.swift
@@ -0,0 +1,10 @@
+
+var singleChar = Character(readLine()!)
+
+print("The entered character is:", singleChar)
+
+var inputChar: Character
+
+inputChar = Character(readLine()!)
+
+print("The entered character is:", inputChar)
