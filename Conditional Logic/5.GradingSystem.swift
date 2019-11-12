5ee8b75c391a050b05b2ec016b27a5bc689ddf34
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-12 07:52:22 +0600

    conditional logic

---
+++ Conditional Logic/5.GradingSystem.swift
@@ -0,0 +1,31 @@
+
+var marks = (Double(readLine()!))!
+
+if (marks >= 80) 
+{
+        print("A+")
+}
+else if (marks >= 70 && marks < 80)
+{
+        print("A")
+}
+else if (marks >= 60 && marks < 70)
+{
+        print("B")
+}
+else if (marks >= 50 && marks < 60)
+{
+        print("C")
+}
+else if (marks >= 40 &&  marks < 50)
+{
+        print("D")
+}
+else if (marks >= 33 && marks < 40)
+{
+        print("E")
+}
+else
+{
+        print("F")
+}
