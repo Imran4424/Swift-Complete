8851f0d652bd3dfbbc15951879e32731b6ba3dd3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-13 07:44:21 +0600

    conditional logic

---
+++ Conditional Logic/7.UpperLower.swift
@@ -0,0 +1,11 @@
+var inputChar = Character(readLine()!)
+
+if(inputChar >= "A" && inputChar <= "Z")
+{
+    print("input character is uppercase")
+}
+
+if(inputChar >= "a" && inputChar <= "z")
+{
+    print("input character is lowercase")
+}
---
+++ Conditional Logic/8.VowelConsonant.swift
@@ -0,0 +1,11 @@
+
+var inputChar = Character(readLine()!)
+
+if("A" == inputChar || "E" == inputChar || "I" == inputChar || "O" == inputChar || "U" == inputChar || "a" == inputChar || "e" == inputChar || "i" == inputChar || "o" == inputChar || "u" == inputChar)
+{
+    print("Vowel")
+}
+else
+{
+    print("Consonant")
+}
