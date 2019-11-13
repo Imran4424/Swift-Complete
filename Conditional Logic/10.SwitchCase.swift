c200a441d30c5299db5b31f93bdbc881fdbe5cf0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-13 23:36:49 +0600

    conditional logic

---
+++ Conditional Logic/10.SwitchCase.swift
@@ -0,0 +1,55 @@
+
+var inputDigit = (Int(readLine()!))!
+
+switch(inputDigit)
+{
+    case 0:
+        print("Zero")
+        break
+    
+    case 1:
+        print("One")
+        break
+        
+    case 2:
+        print("Two")
+        break
+    
+    case 3:
+        print("Three")
+        break
+    
+    case 4:
+        print("Four")
+        break
+    
+    case 5:
+        print("Five")
+        break
+        
+    case 6:
+        print("Six")
+        break
+        
+    case 7:
+        print("Seven")
+        break
+        
+    case 8:
+        print("Eight")
+        break
+        
+    case 9:
+        print("Nine")
+        break
+        
+    default:
+        print("Not a digit")
+        break
+}
+
+/*
+    we can implement switch case conditionals in swift just like C or C++
+    
+    More advantange is we don't have to put semicolons(;) at the end of every statement
+*/
