c517b248183de67ee0f47a8a7a24dac4931f63d7
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-13 07:41:41 +0600

    conditional logic

---
+++ Conditional Logic/6.LeapYear.swift
@@ -0,0 +1,25 @@
+
+var year = (Int(readLine()!))!
+
+if(year % 4 == 0)
+{
+    if(year % 100 == 0)
+    {
+        if(year % 400 == 0)
+        {
+            print("Leap year")
+        }
+        else
+        {
+            print("Not a leap year")
+        }
+    }
+    else
+    {
+        print("Leap year")
+    }
+}
+else
+{
+    print("Not a Leap year")
+}
