094a09e3a00c5a759852947ebfb5705635ed1cde
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-16 14:35:43 +0600

    Function

---
+++ 8.Function/10.DefaultParameters.swift
@@ -0,0 +1,18 @@
+
+// function with default parameters
+
+func Add(a: Int = 0, b: Int = 0, c: Int = 0, d: Int = 0) -> Int
+{
+    return a + b + c + d
+}
+
+
+print("Sum is:", Add())
+
+print("Sum is:", Add(a: 2))
+
+print("Sum is:", Add(a: 2, b: 5))
+
+print("Sum is:", Add(a: 2, b: 5, c: 3))
+
+print("Sum is:", Add(a: 2, b: 5, c: 3, d: 10))
