d2e0ace39c6449ef46ef537a8702f70184eacc12
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-16 15:11:11 +0600

    Function

--- 8.Function/7.MultipleReturnType.swift
+++ 8.Function/7.MultipleReturnType.swift
@@ -42,50 +42,50 @@ print("Total marks:", compoundVariable.2)
 print("Average marks:", compoundVariable.3)
 
 /* 
-    In this code we will learn how to return multiple values from a function
+	In this code we will learn how to return multiple values from a function
 
-    the trick is quite simple
+	the trick is quite simple
 
-    let's consider a function
+	let's consider a function
 
-    func AnalyzingArray(marks : [Int]) -> Int
-    {
+	func AnalyzingArray(marks : [Int]) -> Int
+	{
 
-    }
+	}
 
-    this function has only one return type
+	this function has only one return type
 
-    if we want to have two return type then that will be
+	if we want to have two return type then that will be
 
-    func AnalyzingArray(marks : [Int]) -> (Int, Int)
-    {
+	func AnalyzingArray(marks : [Int]) -> (Int, Int)
+	{
 
-    }
+	}
 
-    see, we are just joining two return types in a single unit using first bracket
+	see, we are just joining two return types in a single unit using first bracket
 
-    here two return types will come as a package of two return types
+	here two return types will come as a package of two return types
 
-    since, they are unlabeled,
-    first return type will be accessible at 0 index
-    second return type will be accessible at 1 index
-    and 
-    the whole compound variable will be accesible at 2 index
+	since, they are unlabeled,
+	first return type will be accessible at 0 index
+	second return type will be accessible at 1 index
+	and 
+	the whole compound variable will be accesible at 2 index
 
-    now, if we want to return four return type
+	now, if we want to return four return type
 
-    func AnalyzingArray(marks : [Int]) -> (Int, Int, Int, Double)
-    {
+	func AnalyzingArray(marks : [Int]) -> (Int, Int, Int, Double)
+	{
 
-    }
+	}
 
-    here, we can see, we are allowed to have different types of multiple return type
+	here, we can see, we are allowed to have different types of multiple return type
 
-    since, they are unlabeled,
-    first return type will be accessible at 0 index
-    second return type will be accessible at 1 index
-    third return type will be accessible at 2 index
-    fourth return type will be accessible at 3 index
-    and 
-    the whole compound variable will be accesible at 4 index
+	since, they are unlabeled,
+	first return type will be accessible at 0 index
+	second return type will be accessible at 1 index
+	third return type will be accessible at 2 index
+	fourth return type will be accessible at 3 index
+	and 
+	the whole compound variable will be accesible at 4 index
 */
--- 8.Function/8.MultipleReturnType.swift
+++ 8.Function/8.MultipleReturnType.swift
@@ -41,3 +41,6 @@ print("Total marks:", compoundVariable.total)
 
 print("Average marks:", compoundVariable.average)
 
+/*  
+	
+*/
