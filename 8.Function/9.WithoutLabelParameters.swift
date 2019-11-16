26e2337ce19b92bc8f7c0ef54dcb427e82edcaf8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-16 15:31:27 +0600

    Function

--- 8.Function/9.WithoutLabelParameters.swift
+++ 8.Function/9.WithoutLabelParameters.swift
@@ -12,5 +12,20 @@ var numTwo = (Int(readLine()!))!
 print("The sum is:", Add(numOne, numTwo))
 
 /* 
-	by default, in swift 
+	by default in swift, function calling needs to mention the label(parameter name in function)
+
+	sometimes this can annoying specially for a C/C++ coder
+
+	for this, swift also allow us to call function without labels
+
+	but for that we need to underscore(_) before  the parameter names in function
+
+	example:
+	
+	func Add( _ x: Int, _ y: Int) -> Int
+	{
+
+	}
+
+	Add(7, 11)
 */
