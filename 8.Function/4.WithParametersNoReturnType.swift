16608591a4509892174735437fd0e01ece35adbf
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-16 14:28:57 +0600

    Function

---
+++ 8.Function/4.WithParametersNoReturnType.swift
@@ -0,0 +1,42 @@
+
+// function with parameters and no return type
+
+func Add(x: Int, y: Int) -> Void
+{
+    print("The sum is:", x + y)
+}
+
+var numOne = (Int(readLine()!))!
+var numTwo = (Int(readLine()!))!
+
+Add(x : numOne, y : numTwo)
+
+/*
+    In this code, we will learn about swift functions
+    
+    swift function syntax is more read able to user
+    
+    basic function syntax
+    
+    func FunctionName(argumentName: argumentType, argumentName: argumentType, .....) -> returnType
+    {
+        /// statements
+    }
+    
+    func keyword is mandatory to create a function
+    
+    but the other things are user-defined
+    
+    example:
+    
+    func Add(x: Int, y: Int) -> Void
+    {
+        print(x + y)
+    }
+    
+    this is a function,
+    whose function name is Add
+    who has two Int type parameters
+    whose has no return type that means Void
+
+*/
