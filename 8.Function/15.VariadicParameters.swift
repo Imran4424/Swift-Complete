40ed69191241bf13b1baf19e7ece6bb83fee7dbf
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-17 13:14:41 +0600

    Function

--- 8.Function/15.VariadicParameters.swift
+++ 8.Function/15.VariadicParameters.swift
@@ -23,3 +23,16 @@ print("Sum is:", Add(number: 2, 5))
 print("Sum is:", Add(number: 2, 5, 3))
 
 print("Sum is:", Add(number: 2, 5, 3, 10))
+
+
+/* 
+    vardiatic parameter is cool feature of swift
+
+    A variadic parameter accepts zero or more values of a specified type. You use a variadic parameter to specify that the 
+    parameter can be passed a varying number of input values when the function is called. Write variadic parameters by inserting 
+    three period characters (...) after the parameter’s type name. 
+
+    read this for more detail
+
+    https://docs.swift.org/swift-book/LanguageGuide/Functions.html
+*/
