6520f805175b5efda219a8f346460c121481b925
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-17 13:03:28 +0600

    Function

--- 8.Function/12.FunctionOverloading.swift
+++ 8.Function/12.FunctionOverloading.swift
@@ -19,5 +19,8 @@ Add(a: 7.2, b: 4.5)
 
         swift function overloading is just same as C++ function overloading
 
-        function overloading is a technique
+        function overloading is a technique in which we declare multiple function of same name with differnt parameter type or
+        parameter number.
+
+        so, overloading depends on two things
 */
