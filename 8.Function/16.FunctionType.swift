f5ae26c57e0007e4a61f08275eaffd18c2664d3d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-17 14:33:25 +0600

    Function

---
+++ 8.Function/16.FunctionType.swift
@@ -0,0 +1,21 @@
+
+
+func AddTwo ( _ a: Int, _ b: Int)
+{
+        return a + b;
+}
+
+func MultiplyThree(_ a: Int, _ b: Int)
+{
+        return a * b
+}
+
+var mathFunction: (Int, Int) -> Int
+
+mathFunction = AddTwo
+
+print("This is addition using func Types:", mathFunction(5, 5))
+
+mathFunction = MultiplyThree
+
+print("This is multipication using func Types:", mathFunction(5, 5))
