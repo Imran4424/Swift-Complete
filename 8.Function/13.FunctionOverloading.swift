6c583b9ea6f771889beb196f2a693a63ea13e929
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-17 13:05:05 +0600

    Function

--- 8.Function/13.FunctionOverloading.swift
+++ 8.Function/13.FunctionOverloading.swift
@@ -13,3 +13,18 @@ func Add(a: Int, b: Int, c: Int) -> Int
 Add(a: 3, b: 4)
 Add(a: 3, b: 4, c: 7)
 
+/*  
+        In this code we will learn about function overloding
+
+        swift function overloading is just same as C++ function overloading
+
+        function overloading is a technique in which we declare multiple function of same name with differnt parameter type or
+        parameter number.
+
+        so, overloading depends on two things
+
+        1. Parameter type
+        2. Parameter number
+
+        here we are using parameter type for function overloading
+*/
