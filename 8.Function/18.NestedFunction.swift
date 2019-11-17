4268960eeb5645bf75c39b0ef9a36ffa03092625
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-17 22:40:32 +0600

    function

--- 8.Function/18.NestedFunction.swift
+++ 8.Function/18.NestedFunction.swift
@@ -1,8 +1,35 @@
 
-func Operation(a: Int, int b : Int)
+func Operation(a: Int, int b : Int) -> Int
 {
-        
+        func Options()
+        {
+                print("1.Addition")
+                print("2.Substraction")
+                print("3.Multipication")
+        }
+
+        Options();
+
+        var command = (Int(readLine()!))!
+
+        if(1 == command)
+        {
+                return a + b
+        }
+        else if(2 == command)
+        {
+                return a - b
+        }
+        else if(3 == command)
+        {
+                return a * b
+        }
 }
+
+var numOne = (Int(readLine()!))!
+var numTwo = (Int(readLine()!))!
+
+print("The result is:", Operation(a: x, b: y))
 
 
 /* 
