a1684ae62a8f9a8cd22855b33e5e2bc14f5cbb06
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-17 13:11:03 +0600

    Function

--- 8.Function/14.DefaultParameters.swift
+++ 8.Function/14.DefaultParameters.swift
@@ -19,4 +19,17 @@ print("Sum is:", Add(a: 2, b: 5, c: 3, d: 10))
 
 /*  
 	In this code, we will learn about default parameters
+
+	swift default parameters are just like C++ default parameters
+
+	default parameters are used to overload function in the term of parameter number
+
+	In this code,
+	we using default parameters for overloading functions using the parameter number
+	0
+	1
+	2
+	3
+	4
+
 */
