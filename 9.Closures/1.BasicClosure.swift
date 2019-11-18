ebf7308181909ac9939c4c891bbe7c30d19ad72a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-11-18 07:25:31 +0600

    function

--- 9.Closures/1.BasicClosure.swift
+++ 9.Closures/1.BasicClosure.swift
@@ -8,6 +8,7 @@ let closureOne = { }
 // this is a very simple closure
 // which contains one simple statement
 // this closure has no parameters and no return type
+
 let closureTwo =
 {
         print("I am a programmer")
