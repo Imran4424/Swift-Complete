fc2955391d9f3c60306611717af723fce6bfa562
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-12-01 15:56:49 +0600

    optionals

--- 1.Input Output/15.Optionals.swift
+++ 1.Input Output/15.Optionals.swift
@@ -2,4 +2,7 @@
         optionals is a special type of varibale which might or might not have a value
 
         just think optionals as a glass which might have water or might not have water(empty glass)
+
+        there are two ways we can declare a variable
 */
+
