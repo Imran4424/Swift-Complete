c8c7934c8324b24afb4e40fd525b342fb2839632
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-12-01 19:45:28 +0600

    optionals

--- 1.Input Output/16.Optionals.swift
+++ 1.Input Output/16.Optionals.swift
@@ -7,32 +7,40 @@
 /*  
         there are two types optional variables
 
-        In this code we will only explore implicitly unwrapped optionals
+        In this code we will only explore explicitly unwrapped optionals
 */
 
 // this is a Int type optional
 
-var number: Int! = nil
+var number: Int? = nil
 
 print("This optionals value is:", number)
 
 // this is a Double type optional
 
-var points : Double!
+var points : Double?
 
 print("This optionals value is:", points)
 
 // this is a int type optional which has a value
 
-var price: Int! = 50
+var price: Int? = 50
 
-// this is a explicitly unwrapped optional, we can't use it like a variable
+// we can't use it like a variable
 
 print("This optionals value is: \(price)")
 
 // to use it like a variable we need to unwrapped it using the exclamation mark(!) 
 
-print("This optionals value is:", price!)
+print("This optionals value is: \(price!)")
+
+
+let possibleString: String? = "An optional string."
+
+// without the exclamation mark we will get error for assigning cause this is a explicitly unwrapped optionals
+
+let forcedString: String = possibleString! // requires an exclamation mark
+
 
 /*  
         optionals may or may not contain any value
