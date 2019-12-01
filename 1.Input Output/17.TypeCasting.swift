b7914e56bb93720d2baa76b65200b32402993a12
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-12-01 21:17:18 +0600

    optionals

--- 1.Input Output/17.TypeCasting.swift
+++ 1.Input Output/17.TypeCasting.swift
@@ -1,4 +1,12 @@
 
 var numberString : String = "1234"
 
+print("The string value is:", numberString)
+
 var numberInt : Int = Int(numberString)
+
+print("The Interger value is:", numberInt)
+
+var numberDouble: Double = Double(numberInt)
+
+print("The Double value is:", numberDouble)
