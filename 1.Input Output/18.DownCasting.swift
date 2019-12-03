2350d8fa47b34a237950309765235507aa903785
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-12-03 07:37:54 +0600

    input output

--- 1.Input Output/18.DownCasting.swift
+++ 1.Input Output/18.DownCasting.swift
@@ -2,7 +2,7 @@
 /*  
         In this code, we will learn about downcast
 
-        when we downcast an object, we are not sure that
+        when we downcast an object, we are not sure that this will succeed
 
         Use the conditional form of the type cast operator (as?) when you are not sure if the downcast will succeed.
         This form of the operator will always return an optional value, and the value will be nil if the downcast was not 
