adcfabb25aa8d89fc8156fb3060f467adf3c5419
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-19 23:48:38 +0600

    properties

--- 2.Properties/5.ComputedProperty.swift
+++ 2.Properties/5.ComputedProperty.swift
@@ -3,3 +3,26 @@
 
         code example: https://docs.swift.org/swift-book/LanguageGuide/Properties.html
 */
+
+struct Point {
+        var x = 0.0, y = 0.0
+}
+struct Size {
+        var width = 0.0, height = 0.0
+}
+struct Rect {
+        var origin = Point()
+        var size = Size()
+        var center: Point {
+                get {
+                        let centerX = origin.x + (size.width / 2)
+                        let centerY = origin.y + (size.height / 2)
+                        return Point(x: centerX, y: centerY)
+                }
+                set(newCenter) {
+                        origin.x = newCenter.x - (size.width / 2)
+                        origin.y = newCenter.y - (size.height / 2)
+                }
+        }
+}
+
