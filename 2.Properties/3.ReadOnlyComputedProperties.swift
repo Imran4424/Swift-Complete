8bf13a5624dfcaf1013fee7a117f98a697203449
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-19 22:14:01 +0600

    properties

--- 2.Properties/3.ReadOnlyComputedProperties.swift
+++ 2.Properties/3.ReadOnlyComputedProperties.swift
@@ -4,3 +4,14 @@
         code example: https://docs.swift.org/swift-book/LanguageGuide/Properties.html
 */
 
+struct Cuboid {
+        var width = 0.0, height = 0.0, depth = 0.0
+
+        var volume: Double {
+                return width * height * depth
+        }
+}
+
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
+// Prints "the volume of fourByFiveByTwo is 40.0"
