056967809a984c38add7da11eb89da9b9b603d63
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-20 00:10:45 +0600

    properties

--- 2.Properties/5.ComputedProperty.swift
+++ 2.Properties/5.ComputedProperty.swift
@@ -7,9 +7,11 @@
 struct Point {
         var x = 0.0, y = 0.0
 }
+
 struct Size {
         var width = 0.0, height = 0.0
 }
+
 struct Rect {
         var origin = Point()
         var size = Size()
@@ -19,7 +21,7 @@ struct Rect {
                         let centerY = origin.y + (size.height / 2)
                         return Point(x: centerX, y: centerY)
                 }
-                
+
                 set(newCenter) {
                         origin.x = newCenter.x - (size.width / 2)
                         origin.y = newCenter.y - (size.height / 2)
