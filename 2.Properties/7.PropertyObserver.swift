b4ce0417f6ff916eecb62f4f25b68c179dd12339
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-20 00:55:43 +0600

    properties

--- 2.Properties/7.PropertyObserver.swift
+++ 2.Properties/7.PropertyObserver.swift
@@ -7,5 +7,13 @@
 
         property observer can be applied to, stored properties (exception lazy stored property)
 
-        property observers can also be applied to any inherited property (both stored or computed)
+        property observers can also be applied to any inherited property (both stored or computed)  by overriding the 
+        property within a subclass
+
+        there are two property observers in swift
+                willSet
+                didSet
+
+        willSet is called just before the value is stored.
+        didSet is called immediately after the new value is stored.
 */
