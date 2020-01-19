fd13aedc44bd688ef7225008f2d06fb105b96005
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-19 22:20:01 +0600

    properties

--- 2.Properties/4.ReadOnlyComputedProperties.swift
+++ 2.Properties/4.ReadOnlyComputedProperties.swift
@@ -2,3 +2,17 @@
 /** 
         Computed Properties
 */
+
+class Box {
+        var length: Double
+        var width: Double
+        var heigth: Double
+
+        var area: Double {
+                return length * width
+        }
+
+        var Volume: Double {
+                return length * width * heigth
+        }
+}
