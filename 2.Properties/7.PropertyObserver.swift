98383afa6a6e6a9fbdf56f0723f8baa498146a65
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-20 01:00:32 +0600

    properties

--- 2.Properties/7.PropertyObserver.swift
+++ 2.Properties/7.PropertyObserver.swift
@@ -34,13 +34,13 @@
         }
     }
     
-    let stepCounter = StepCounter()
-    stepCounter.totalSteps = 200
-    // About to set totalSteps to 200
-    // Added 200 steps
-    stepCounter.totalSteps = 360
-    // About to set totalSteps to 360
-    // Added 160 steps
-    stepCounter.totalSteps = 896
-    // About to set totalSteps to 896
-    // Added 536 steps
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+// About to set totalSteps to 200
+// Added 200 steps
+stepCounter.totalSteps = 360
+// About to set totalSteps to 360
+// Added 160 steps
+stepCounter.totalSteps = 896
+// About to set totalSteps to 896
+// Added 536 steps
