5dd8f73cedfb4dbba836d4917d4a6a5b495ac2f6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-20 00:48:57 +0600

    properties

--- 2.Properties/7.PropertyObserver.swift
+++ 2.Properties/7.PropertyObserver.swift
@@ -5,6 +5,5 @@
         observers are called every time a property’s value is set, even if the new value 
         is the same as the property’s current value.
 
-        property observer can be applied to,
-        
+        property observer can be applied to, stored properties
 */
