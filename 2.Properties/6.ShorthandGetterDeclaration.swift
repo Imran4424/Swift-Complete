4f124715463a137a5c6c61c2e014562005ae2631
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-20 00:30:25 +0600

    properties

--- 2.Properties/6.ShorthandGetterDeclaration.swift
+++ 2.Properties/6.ShorthandGetterDeclaration.swift
@@ -16,6 +16,8 @@ struct Rect {
         var origin = Point()
         var size = Size()
         var center: Point {
+                // when getter have single statement
+                // we can ommit the return statement
                 get {
                         Point(x: origin.x + (size.width / 2), y: origin.y + (size.height / 2))
                 }
