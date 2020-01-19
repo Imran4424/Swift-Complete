82080bf1a275be250a840adb31581e0daaaf5b06
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-19 21:04:12 +0600

    properties

--- 2.Properties/1.StoredProperties.swift
+++ 2.Properties/1.StoredProperties.swift
@@ -1,9 +1,15 @@
 /** 
         Stored Properties
+
+        code example: https://docs.swift.org/swift-book/LanguageGuide/Properties.html
 */
 
 
 
 /** 
         Stored Properties are normal C++ type variable or constant
+
+        lazy stored property is a property
+
+
 */
