3fa61ca70d354f79ab2593c5d974edf4574b6ca9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-19 21:16:41 +0600

    properties

--- 2.Properties/2.LazyStoredProperty.swift
+++ 2.Properties/2.LazyStoredProperty.swift
@@ -4,6 +4,27 @@
         code example: https://docs.swift.org/swift-book/LanguageGuide/Properties.html
 */
 
+class DataImporter {
+        /*
+        DataImporter is a class to import data from an external file.
+        The class is assumed to take a nontrivial amount of time to initialize.
+        */
+        var filename = "data.txt"
+        // the DataImporter class would provide data importing functionality here
+}
+
+class DataManager {
+        lazy var importer = DataImporter()
+        var data = [String]()
+        // the DataManager class would provide data management functionality here
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+// the DataImporter instance for the importer property has not yet been created
+
+
 /**  
         A lazy stored property is a property whose initial value is not calculated until the first time 
         it is used.
