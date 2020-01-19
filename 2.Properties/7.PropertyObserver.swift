37adac5c21ccc36df4bddd25c5907b405029a94b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-20 00:47:13 +0600

    properties

---
+++ 2.Properties/7.PropertyObserver.swift
@@ -0,0 +1,7 @@
+/**  
+        Property Observer
+
+        Property observers observe and respond to changes in a property’s value. Property 
+        observers are called every time a property’s value is set, even if the new value 
+        is the same as the property’s current value.
+*/
