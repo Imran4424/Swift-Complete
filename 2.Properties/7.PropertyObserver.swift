0da29efce6a4516369126f22ce7d9d569a845139
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-20 00:59:48 +0600

    properties

--- 2.Properties/7.PropertyObserver.swift
+++ 2.Properties/7.PropertyObserver.swift
@@ -20,3 +20,26 @@
         code example: https://docs.swift.org/swift-book/LanguageGuide/Properties.html
 */
 
+    class StepCounter {
+        var totalSteps: Int = 0 {
+                willSet(newTotalSteps) {
+                        print("About to set totalSteps to \(newTotalSteps)")
+                }
+
+                didSet {
+                        if totalSteps > oldValue  {
+                                print("Added \(totalSteps - oldValue) steps")
+                        }
+                }
+        }
+    }
+    let stepCounter = StepCounter()
+    stepCounter.totalSteps = 200
+    // About to set totalSteps to 200
+    // Added 200 steps
+    stepCounter.totalSteps = 360
+    // About to set totalSteps to 360
+    // Added 160 steps
+    stepCounter.totalSteps = 896
+    // About to set totalSteps to 896
+    // Added 536 steps
